c5772052786fb25152174b564fbadcff1407294d
Maya Krishnan <mayark527@gmail.com>
2025-11-17 10:37:20 -0600

    Initial Commit

---
+++ Pig/PigApp.swift
@@ -0,0 +1,17 @@
+//
+//  PigApp.swift
+//  Pig
+//
+//  Created by Maya Krishnan on 11/17/25.
+//
+
+import SwiftUI
+
+@main
+struct PigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
